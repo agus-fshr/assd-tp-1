6abb82a6831e5d159e2372bdc1bd1c1ba2d3aaec
Mariano Oms <moms@itba.edu.ar>
2024-03-15 20:24:21 -0300

    front funciona(?)

--- mywindow.py
+++ mywindow.py
@@ -9,23 +9,55 @@ class mywindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         
     #extrae el número ingresado
-    def getnum(self, lineedit):
+    def getNum(self, lineedit):
         try:
             return float((lineedit.text()).replace(',','.'))
         except:
             return 0
             
     #extrae indice
-    def getindex(self, combobox):
+    def getIndex(self, combobox):
         return combobox.currentIndex()
     
     #returns multiplier
-    def get_multiplier(self, combobox):
+    def getMultiplier(self, combobox, flag):
         comboboxindex = combobox.currentIndex()
-        if comboboxindex == 0:
-            multiplier = 1
-        elif comboboxindex == 1:
-            multiplier = 1000
+        if flag == 0:               #si el flag es 0 entonces es frecuencia
+            if comboboxindex == 0:
+                multiplier = 1
+            elif comboboxindex == 1:
+                multiplier = 1000
+        else:
+            if comboboxindex == 0:
+                multiplier = 1
+            elif comboboxindex == 1:
+                multiplier = 0.001
         return multiplier
+
+    #devuelve el tab en el cual estas
+    def getTabIndex(self):
+        return self.TabWidget.currentIndex()
     
+    #retunea que esta prendido
+    def getEnable(self):
+        l=["Faa","SH","llaveAnal","Fr"]
+        v = [
+            self.faaenable.isChecked(),
+            self.shenabale.isChecked(),
+            self.llaveanalenable.isChecked(),
+            self.frenable.isChecked()
+        ]
+        return dict(zip(l,v))
     
+    #returnea los parametros de entrada
+    def getXin(self):
+        l=["tipo","frec","frec multi", "amplitud", "amplitud multi","duty"]
+        v=[
+            self.getIndex(self.xinbox),             #0 senoidal, 1 cuadrada, 2 triangular
+            self.getNum(self.xinfrecline),
+            self.getMultiplier(self.xinfrecbox,0),
+            self.getNum(self.xinampline),
+            self.getMultiplier(self.xinampbox,1),
+            self.getNum(self.xindutyline)
+        ]
+        return dict(zip(l,v))
